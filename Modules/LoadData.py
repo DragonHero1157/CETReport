8b9d05e26309635f9a44548fd0b3ae67c60603fc
Philip Chu (Red Hat) <phchu@redhat.com>
2025-12-11 13:51:38 -0500

    Added docstring documentation for the module and its functions.

--- Modules/LoadData.py
+++ Modules/LoadData.py
@@ -1,10 +1,23 @@
 """
 Docstring for Modules.LoadData
+    Contains the functions that perform the 'load' of the transformed data
+    Typically, the 'load' function of an ETL process is into a database. However in this case the 'load' is to a new CSV file.
 """
 import csv
 from Modules import GlobalVars
 
 def load_Data(transformedBuffer: dict, myArgv):
+    """
+    Docstring for load_Data
+        Writes the transformed data to a comma delimited text file after writing a header record
+        Uses the Python library csv class to easily format the output
+    
+    Args:
+        dict: dictionary of associate progress report records
+
+    Returns:
+        None
+    """
     # Create new CSV file from transformed data
     # Format header record by defining field names
     # print(f'reportAsOfDate = {reportAsOfDate}')
